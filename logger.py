e22e8e474c5ee9482770208b04069d86d2de30e3
Vladick <72vladvlad72@gmail.com>
2023-09-12 12:50:16 +0500

    fixLogging

--- logger.py
+++ logger.py
@@ -2,4 +2,5 @@ import logging
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 @staticmethod
 def Log(text:str):
+     print(text)
      logging.info(text)
